d0abea85fa603db57dd5e359e13f67d03d42d7bd
Istiak Rahman <istiak.rahman1020@gmail.com>
2024-10-02 01:55:34 +0600

    Class-1 done

---
+++ C Programming/class 1/class1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    //Data Types 
+    // int age=18; // %d
+    // float money = 100.14; // %f
+    // char character = 'A'; // %c
+
+    //Output
+    // printf("Hello I am a programmer!!\n");
+    // printf("My age is %d", age);
+    // printf("My balance is %f\n", money);
+    // printf("B is after %c\n",character);
+
+    // printf("I am age %d, I am money %f, I am character %c.\n I am all %c %f %d", age, money,character, character, money,age );
+
+
+    //Input
+    // constant "const"
+  int balance =10000000;
+  
+
+//    printf("What's your age : ");
+//    scanf("%d",&age);
+   printf("My balance is %d\n",balance );
+   balance=0;
+   printf("My age is %d",balance );
+    
+}
