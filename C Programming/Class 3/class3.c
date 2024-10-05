8a43c7f28efe15c43f80daf6a1597fb0840d480e
Istiak Rahman <istiak.rahman1020@gmail.com>
2024-10-05 23:21:40 +0600

    class 3

---
+++ C Programming/Class 3/class3.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+int main(){
+
+    // int himel=17, fahim = 18;
+
+    // printf("Himel = %d, Fahim = %d\n ", himel, fahim);
+
+    // if(himel == fahim){// ==, !=, <, > , <=, >=, &&, ||, !
+
+    //     // printf("15 = 10");
+    //     printf("Himel equal to Fahim\n");
+
+    // }else if(himel >fahim){
+    //     printf("Himel > Fahim\n");
+
+
+    // }else if(himel < fahim){
+    //             printf("Himel < Fahim\n");
+    // }
+    // else{
+    //     printf("Himel not equal to Fahim");
+    //     // printf("Ops 15 not equal to 10.");
+    // } 
+
+
+    // int a=33, b= 33;
+    // if(a>=b){
+    //     printf("Yes\n");
+    // }else if(a<=b){
+    //     printf("OPS\n");
+    // }
+
+    // int x=3,y=7,z=9;
+    // if (x>y && x>z)//Then x 
+    // {
+    //    printf("X\n");
+    // }else if (y>x && y>z )//Then y
+    // {
+    //          printf("Y\n");
+
+        
+    // }else//Then z
+    // {
+    //          printf("Z\n");
+       
+    // }
+    
+    // int gamePlay= 3, win = 0, lose = 3;
+
+    // if (gamePlay>=5)
+    // {
+    //   if(win>=3){
+        
+    //     printf("WIN\n");
+    //   }else{
+    //     printf("LOSE\n");
+    //   }
+
+    // }else
+    // {
+    //     if (lose>=2)
+    //     {
+    //         printf("LOSE\n");
+    //     }else{
+    //           printf("WIN\n");
+    //     }
+        
+    // }
+    
+    for(int i=1;i<=20; i++ ){
+
+        if(i%2!=0){
+            continue;
+        }  if(i%2==0){
+            continue;
+        }
+        printf("%d\n",i);
+        // if(i==5){
+        //     break;
+        // }  printf("%d\n",i);
+        // if(i==5){
+        //     break;
+        // }  printf("%d\n",i);
+        // if(i==5){
+        //     break;
+        // }  printf("%d\n",i);
+        // if(i==5){
+        //     break;
+        // }  printf("%d\n",i);
+        // if(i==5){
+        //     break;
+        // }
+    }
+
+    
+    
+    return 0;
+}
